72237ec95596d21e7e15072fbef8c1ee89448ed0
nicolasgeronimorodi <nicolasgeronimorodi@gmail.com>
2022-11-06 21:26:44 -0300

    interfaz e implementacion de dao para corroborar credenciales

---
+++ VS/DatosCarrera/datos/Implementaciones/LoginDAO.cs
@@ -0,0 +1,34 @@
+﻿using DatosCarrera.datos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosCarrera.datos.Implementaciones
+{
+    public class LoginDAO:IloginUsuario
+    {
+
+
+
+   
+        public bool ChequearLogin(string usuario, string contrasenia)
+        {
+            string sp = "select usuario, contrasenia FROM Administradores where usario=@usuario and contrasenia=@contrasenia";
+            List<Parametro> lst = new List<Parametro>();
+            lst.Add(new Parametro("@usuario", usuario));
+            lst.Add(new Parametro("@contrasenia", contrasenia));
+
+            DataTable tabla = DBHelper.ObtenerInstancia().ConsultaSQL(sp, lst);
+            
+            return tabla.Rows.Count > 0;
+
+
+
+
+        }
+    }
+}
---
+++ VS/DatosCarrera/datos/Interfaces/IloginUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosCarrera.datos.Interfaces
+{
+    public interface IloginUsuario
+    {
+        bool ChequearLogin(string usuario, string contrasenia);
+
+
+    }
+}
